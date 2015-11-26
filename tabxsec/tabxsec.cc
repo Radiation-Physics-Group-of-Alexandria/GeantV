084787244239f5d7d97ecf2fbeeb21b824335886
Federico Carminati <federico.carminati@cern.ch>
2015-11-26 12:08:33 +0100

    GEANT-133 Serialisation of TPartIndex

--- tabxsec/tabxsec.cc
+++ tabxsec/tabxsec.cc
@@ -639,15 +639,11 @@ int main(int argc, char **argv) {
       // Store the particle table into PartIndex
       TPartIndex::I()->SetPartTable(pPDG, np);
       // Set PDG to GV code map
-    #ifdef USE_VECGEOM_NAVIGATOR 
-      vecgeom::map<G4int, G4int> pdgToGVmap;
-    #else
-      std::map<G4int, G4int> pdgToGVmap;
-    #endif 
-      for (G4int i = 0; i < np; ++i)
-        pdgToGVmap[pPDG[i]] = i;
+      //      std::map<G4int, G4int> pdgToGVmap;
+      //for (G4int i = 0; i < np; ++i)
+      //  pdgToGVmap[pPDG[i]] = i;
 
-      TPartIndex::I()->SetPDGToGVMap(pdgToGVmap);
+      //TPartIndex::I()->SetPDGToGVMap(pdgToGVmap);
 
       // Check that "pions will be pions"
       printf("Code %d part %s\n", 211, TPartIndex::I()->PartName(TPartIndex::I()->PartIndex(211)));
