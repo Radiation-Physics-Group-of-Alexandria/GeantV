2d58df2dde5e2d71cd17079fc8973a8279885b4e
Philippe Canal <pcanal@fnal.gov>
2015-12-04 15:20:01 -0600

    GeantPropagator must be created _after_ the coprocessor section to get the number of thread right

--- vecprot_v2/runCMS.C
+++ vecprot_v2/runCMS.C
@@ -53,6 +53,7 @@ void runCMS(int ncputhreads=4,
 
    //  Enable use of RK integration in field for charged particles
    prop->fUseRungeKutta = false;
+
    // prop->fEpsilonRK = 0.001;  // Revised / reduced accuracy - vs. 0.0003 default 
 
    if (broker) prop->SetTaskBroker(broker);
