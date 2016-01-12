4683875fa304e2a22272a6897cafa8d28ab86be9
John Apostolakis <john.apostolakis@cern.ch>
2016-01-12 19:19:24 +0100

    Added new class FieldPropagatorFactory, and used it.  And small revisions.
    - FieldPropagatorFactory: Given field class and object (or driver) create Field Propagator
    - GUFieldPropagatorPool:  Added flag and IsInitialized() method
    - GeantPropagator:        Renamed CreateThreadRkPropagator() to ObtainThreadRkPropagator()
    - testGUPropagateField:   Revision to use (FieldPropagation) Factory

---
+++ magneticfield/inc/FieldPropagatorFactory.h
@@ -0,0 +1,108 @@
+//===----------------------------------------------------------------------===//
+/**
+ * @file   StepperFactory.h
+ * @brief  Abstract field class for Geant-V prototype
+ * @author John Apostolakis
+ * @date   12 January 2016
+ */
+//===----------------------------------------------------------------------===//
+
+#ifndef FIELDPROPAGATOR_FACTORY_H
+#define FIELDPROPAGATOR_FACTORY_H 1
+
+#include <ostream>
+
+#include "GUFieldPropagator.h"
+#include "GUFieldPropagatorPool.h"
+
+// template<typename Field_t> // , typename Equation_t>
+class FieldPropagatorFactory
+{
+  public:
+   
+  static constexpr unsigned int  Nvar = 6; // Integration will occur over 3-position & 3-momentum coord.
+  // using Equation_t = TMagFieldEquation<Field_t,Nvar>;
+
+  // Initialise the classes required for tracking in field
+
+  static constexpr double   fDefaultMinStep      = 0.0001;  // = 1 micron
+  static constexpr double   fDefaultEpsTolerance = 1.0e-4;
+
+  static GUFieldPropagator* CreatePropagator( // Field_t&    gvField,
+                          //   Equation_t* gvEquation=  nullptr,
+                               GUIntegrationDriver&   integrDriver,
+                               double                 relTol= fDefaultEpsTolerance);
+   // The GUIntegrationDriver object which is passed must be on the heap.
+   //  It will be owned by the Propagator
+
+  template<typename Field_t> // , typename Equation_t>
+  static GUFieldPropagator* CreatePropagator(Field_t&    gvField,
+                               double      relativeEpsTolerance,
+                               double      minStepSize= fDefaultMinStep);
+   // The Field_t object which is passed must be on the heap.
+   //  It will be owned by the Propagator
+
+  static void RegisterPropagator(GUFieldPropagator*);
+};
+
+// template<typename Field_t> // , typename Equation_t>
+GUFieldPropagator*
+FieldPropagatorFactory::CreatePropagator( // Field_t&              gvField,
+                                          GUIntegrationDriver&  integrDriver,
+                                          double                relEpsilonTolerance)
+{
+  // using Equation_t =  TMagFieldEquation<Field_t,Nvar>;
+  GUFieldPropagator* fieldPropagator = nullptr;
+
+  // constexpr double epsTol = 3.0e-4;               // Relative error tolerance of integration
+  // int  statisticsVerbosity= 0;
+  cout << "Parameters for RK integration in magnetic field: "; //  << endl;
+
+  // GUFieldPropagator *
+  fieldPropagator =
+     new GUFieldPropagator(&integrDriver, relEpsilonTolerance);  // epsTol);
+  cout << " - Integration constraint:  eps_tol= " << relEpsilonTolerance << endl;
+
+  RegisterPropagator(fieldPropagator);
+
+  return fieldPropagator;
+}
+
+template<typename Field_t> // , typename Equation_t>
+GUFieldPropagator*
+FieldPropagatorFactory::CreatePropagator(Field_t& gvField,
+                                         double   relEpsilonTolerance,
+                                         double   minStepSize)
+{
+  using Equation_t =  TMagFieldEquation<Field_t,Nvar>;
+
+  auto gvEquation = 
+     FieldEquationFactory::CreateMagEquation<Field_t>(&gvField);
+  cout << "Parameters for RK integration in magnetic field: "; //  << endl;
+  cout << " - Driver minimum step (h_min) = " << minStepSize << endl;
+
+  auto // GUVIntegrationStepper*
+     aStepper = StepperFactory::CreateStepper<Equation_t>(gvEquation); // Default stepper
+
+  int   statisticsVerbosity= 0;
+  auto integrDriver = new GUIntegrationDriver( minStepSize,
+                                               aStepper,
+                                               Nvar,
+                                               statisticsVerbosity);
+
+  return CreatePropagator( *integrDriver, relEpsilonTolerance );
+}
+
+// template<typename Field_t, typename Equation_t>
+void
+FieldPropagatorFactory::RegisterPropagator(GUFieldPropagator* fieldPropagator)
+{
+  GUFieldPropagatorPool* fpPool= GUFieldPropagatorPool::Instance();
+  assert( fpPool );  // Cannot be zero
+  if( fpPool ) {
+     fpPool->RegisterPrototype( fieldPropagator );
+  } else {
+     ::Error("PrepareRkIntegration","Cannot find GUFieldPropagatorPool Instance.");
+  }
+}
+#endif
