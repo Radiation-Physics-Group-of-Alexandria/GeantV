ad213cdcb1517f0077ad7405653c122dc6c7ce4b
Ananya <ananya.ananya@cern.ch>
2016-01-19 18:05:59 +0100

    files added for vectorization of equation of motion. Not complete yet.

--- magneticfield/inc/GUVMagneticField.h
+++ magneticfield/inc/GUVMagneticField.h
@@ -32,16 +32,4 @@ class GUVMagneticField : public GUVField
     GUVMagneticField& operator = (const GUVMagneticField &p);
     //  Copy 'standard' components ...
 };
-
-void
-GUVMagneticField::GetFieldValue( const double  Point[4],     // The old interface
-                                 double* FieldArr )
-{
-   vecgeom::Vector3D<double> PositionV3D( Point[0], Point[1], Point[2]);
-   vecgeom::Vector3D<float>  Field_v3f;
-   this->GetFieldValue( PositionV3D, Field_v3f );
-   FieldArr[0]= Field_v3f.x();
-   FieldArr[1]= Field_v3f.y();
-   FieldArr[2]= Field_v3f.z();
-}
 #endif
---
+++ magneticfield/src/GUVVectorMagneticField.cxx
@@ -0,0 +1,12 @@
+//
+// First implementation class for GUVField
+//
+// Used to enable creation of vtable.
+// -------------------------------------------------------------------
+
+#include "GUVVectorMagneticField.h"
+#include <iostream>
+ 
+GUVVectorMagneticField::~GUVVectorMagneticField()
+{
+}
