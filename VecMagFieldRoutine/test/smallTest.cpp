b050429c2c78eae9c48b95a7407efd4ec4912ccf
John Apostolakis <john.apostolakis@cern.ch>
2016-01-27 19:38:43 +0100

    Use helix for small chord (angle < 0.05 rad). Print methods to debug.
    CMSMagField:         Changed to GeantV units - cm, KiloGauss
    GUIntegrationDriver: Removed 'dchord_step' as argument.
    GUTCashKarpRKF45:    New methods to print dy/dx, field for debugging.
    TMagFieldEquation:   Additional methods RightHandSide, GetField methods
    GeantTrack:          Use Helix for small angles < 0.05 (as well as large > 3 pi)
    
    Add smallTest, which computes CMS B-field for one position.

---
+++ VecMagFieldRoutine/test/smallTest.cpp
@@ -0,0 +1,65 @@
+//===--- BenchmarkTiming.cpp - Geant-V --------------------------*- C++ -*-===//
+//
+//                     Geant-V Prototype
+//
+//===----------------------------------------------------------------------===//
+/**
+ * @file  BenchmarkTiming.cpp
+ * @brief Benchmark of implementations of bi-linear interpolation of CMS field
+ * @author Ananya
+ */
+//===----------------------------------------------------------------------===//
+
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cmath> //for sqrt
+// #include <stdlib.h>
+#include <cstdlib>
+
+#include <numeric>
+#include <string>
+#include <functional>
+
+#include <Vc/Vc>
+
+#include "backend/vc/Backend.h"
+#include "backend/vcfloat/Backend.h"
+#include "base/Vector.h"
+
+#include "base/Vector3D.h"
+#include "base/SOA3D.h"
+#include "base/Global.h"
+
+// #include "MagField.h"
+
+#include "CMSmagField.h"
+
+using namespace std;
+
+typedef vecgeom::Vector3D<float> ThreeVector; //normal Vector3D
+typedef vecgeom::Vector3D<vecgeom::kVcFloat::precision_v> ThreeVecSimd_t;
+typedef vecgeom::Vector<float> VcVectorFloat;
+
+
+
+
+int main(){
+
+    CMSmagField m1;
+
+    m1.ReadVectorData("../VecMagFieldRoutine/cms2015.txt");
+    vecgeom::Vector3D<float> position, xyzField;
+
+    std::cout<<"Give x,y and z"<<std::endl;
+    std::cin>>position.x()>>position.y()>>position.z();
+
+    m1.GetFieldValue<vecgeom::kScalarFloat>(position, xyzField);
+
+    std::cout<<"Magnetic Field at "<<position<<" is: "<<xyzField<<" tesla."<<std::endl;
+
+}
+
+
