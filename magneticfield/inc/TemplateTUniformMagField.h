894fe2caed4f3e3089fd0826389bed2ebb0a1afb
Ananya <ananya.ananya@cern.ch>
2016-03-03 17:35:50 +0100

    Indentedation 4->2

--- magneticfield/inc/TemplateTUniformMagField.h
+++ magneticfield/inc/TemplateTUniformMagField.h
@@ -16,84 +16,84 @@
 template <class Backend>
 class TemplateTUniformMagField : public TemplateGUVMagneticField<Backend>
 {
-    public:  // with description
+  public:  // with description
 
-      typedef typename Backend::precision_v Double_v;
-      
-        TemplateTUniformMagField(const vecgeom::Vector3D<double>& FieldVector )
-           : TemplateGUVMagneticField<Backend>() //NumberOfComponents(3)
-            // A field with value equal to FieldVector.
-        {
-           fFieldComponents = FieldVector;
-        }
+    typedef typename Backend::precision_v Double_v;
+    
+    TemplateTUniformMagField(const vecgeom::Vector3D<double>& FieldVector )
+       : TemplateGUVMagneticField<Backend>() //NumberOfComponents(3)
+        // A field with value equal to FieldVector.
+    {
+      fFieldComponents = FieldVector;
+    }
 
-        TemplateTUniformMagField(double vField,
-                                 double vTheta,
-                                 double vPhi  );
+    TemplateTUniformMagField(double vField,
+                             double vTheta,
+                             double vPhi  );
 
-        // virtual
-        ~TemplateTUniformMagField() {}
+    // virtual
+    ~TemplateTUniformMagField() {}
 
-        TemplateTUniformMagField(const TemplateTUniformMagField &p)   // : G4MagneticField(p)
-        {
-           fFieldComponents = p.fFieldComponents;
-        }
+    TemplateTUniformMagField(const TemplateTUniformMagField &p)   // : G4MagneticField(p)
+    {
+      fFieldComponents = p.fFieldComponents;
+    }
 
-        TemplateTUniformMagField& operator = (const TemplateTUniformMagField &p)
-            // Copy constructor and assignment operator.
-        {
-           if (&p == this) return *this;
-           // for (int i=0; i<3; i++) fFieldComponents[i] = p.fFieldComponents[i];
-           fFieldComponents = p.fFieldComponents;
-           return *this;
-        }
+    TemplateTUniformMagField& operator = (const TemplateTUniformMagField &p)
+        // Copy constructor and assignment operator.
+    {
+      if (&p == this) return *this;
+      // for (int i=0; i<3; i++) fFieldComponents[i] = p.fFieldComponents[i];
+      fFieldComponents = p.fFieldComponents;
+      return *this;
+    }
 
 /*        // virtual
-        //  need to comment GetFieldValue, otherwise function overloading/repetition with Backend = kScalar
-        void GetFieldValue( const vecgeom::Vector3D<double> &, // Position,
-                                  vecgeom::Vector3D<double> &FieldValue )
-        {
-           FieldValue= fFieldComponents;
-        }*/
+    //  need to comment GetFieldValue, otherwise function overloading/repetition with Backend = kScalar
+    void GetFieldValue( const vecgeom::Vector3D<double> &, // Position,
+                              vecgeom::Vector3D<double> &FieldValue )
+    {
+       FieldValue= fFieldComponents;
+    }*/
 
-        void GetFieldValue( const vecgeom::Vector3D<Double_v> &, // Position,
-                                  vecgeom::Vector3D<Double_v> &FieldValue )
-        {
-           for (int i=0; i<3; i++) FieldValue[i] = fFieldComponents[i];
-           // FieldValue= fFieldComponents;
-        }
+    void GetFieldValue( const vecgeom::Vector3D<Double_v> &, // Position,
+                              vecgeom::Vector3D<Double_v> &FieldValue )
+    {
+      for (int i=0; i<3; i++) FieldValue[i] = fFieldComponents[i];
+     // FieldValue= fFieldComponents;
+    }
 
-        void SetFieldValue(const vecgeom::Vector3D<double>& fieldValue)
-        {
-           fFieldComponents= fieldValue;
-        }
+    void SetFieldValue(const vecgeom::Vector3D<double>& fieldValue)
+    {
+      fFieldComponents= fieldValue;
+    }
 
-        vecgeom::Vector3D<double> GetConstantFieldValue() const
-        {
-           return fFieldComponents;
-        }
-        // Return the field value
+    vecgeom::Vector3D<double> GetConstantFieldValue() const
+    {
+      return fFieldComponents;
+    }
+    // Return the field value
 
-        // virtual
-        TemplateTUniformMagField* Clone() const
-        {
-           return new TemplateTUniformMagField( *this );
-        }
+    // virtual
+    TemplateTUniformMagField* Clone() const
+    {
+      return new TemplateTUniformMagField( *this );
+    }
 
-        TemplateTUniformMagField* CloneOrSafeSelf( bool /*Safe = 0*/ )
-        // {  Safe= true; return this; }  //  Class is thread-safe, can use 'self' instead of clone
-        // { Safe= false; return new TemplateTUniformMagField( this ); }  // Check ...
-        { /*Safe= false;*/ return Clone(); }  // Check ...
+    TemplateTUniformMagField* CloneOrSafeSelf( bool /*Safe = 0*/ )
+    // {  Safe= true; return this; }  //  Class is thread-safe, can use 'self' instead of clone
+    // { Safe= false; return new TemplateTUniformMagField( this ); }  // Check ...
+    { /*Safe= false;*/ return Clone(); }  // Check ...
 
-        TemplateTUniformMagField* CloneOrSafeSelf( bool* pSafe )
-        {
-           if( pSafe ) *pSafe= true;
-           return this; // ->CloneOrSafeSelf(*pSafe);
-        }
-        //  Class is thread-safe, can use 'self' instead of clone
+    TemplateTUniformMagField* CloneOrSafeSelf( bool* pSafe )
+    {
+      if( pSafe ) *pSafe= true;
+      return this; // ->CloneOrSafeSelf(*pSafe);
+    }
+    //  Class is thread-safe, can use 'self' instead of clone
 
-    private:
-        vecgeom::Vector3D<double> fFieldComponents;
+  private:
+      vecgeom::Vector3D<double> fFieldComponents;
 };
 
 
@@ -102,23 +102,23 @@ TemplateTUniformMagField<Backend>::TemplateTUniformMagField(double vField,
                                                             double vTheta,
                                                             double vPhi     )
 {
-   if ( (vField<0) || (vTheta<0) || (vTheta>Constants::pi) || (vPhi<0) || (vPhi>Constants::twopi) )
-   {
-      // Exception("TemplateTUniformMagField::TemplateTUniformMagField()",
-      //     "GeomField0002", FatalException, "Invalid parameters.") ;
-      std::cerr << "ERROR in TemplateTUniformMagField::TemplateTUniformMagField()"
-                << "Invalid parameter(s): expect " << std::endl;
-      std::cerr << " - Theta angle: Value = " << vTheta
-                << "  Expected between 0 <= theta <= pi = " << Constants::pi << std::endl;
-      std::cerr << " - Phi   angle: Value = " << vPhi
-                << "  Expected between 0 <=  phi  <= 2*pi = " << Constants::twopi << std::endl;
-      std::cerr << " - Magnitude vField: Value = " << vField
-                << "  Expected vField > 0 " << Constants::twopi << std::endl;
-   }
+ if ( (vField<0) || (vTheta<0) || (vTheta>Constants::pi) || (vPhi<0) || (vPhi>Constants::twopi) )
+ {
+    // Exception("TemplateTUniformMagField::TemplateTUniformMagField()",
+    //     "GeomField0002", FatalException, "Invalid parameters.") ;
+    std::cerr << "ERROR in TemplateTUniformMagField::TemplateTUniformMagField()"
+              << "Invalid parameter(s): expect " << std::endl;
+    std::cerr << " - Theta angle: Value = " << vTheta
+              << "  Expected between 0 <= theta <= pi = " << Constants::pi << std::endl;
+    std::cerr << " - Phi   angle: Value = " << vPhi
+              << "  Expected between 0 <=  phi  <= 2*pi = " << Constants::twopi << std::endl;
+    std::cerr << " - Magnitude vField: Value = " << vField
+              << "  Expected vField > 0 " << Constants::twopi << std::endl;
+ }
 
-   //std::sin and cos should work since vTheta etc are always scalar, but what the heck
-   fFieldComponents.Set( vField*Backend::Sin(vTheta)*Backend::Cos(vPhi),
-                         vField*Backend::Sin(vTheta)*Backend::Sin(vPhi),
-                         vField*Backend::Cos(vTheta)                );
+ //std::sin and cos should work since vTheta etc are always scalar, but what the heck
+ fFieldComponents.Set( vField*Backend::Sin(vTheta)*Backend::Cos(vPhi),
+                       vField*Backend::Sin(vTheta)*Backend::Sin(vPhi),
+                       vField*Backend::Cos(vTheta)                  );
 }
 #endif
