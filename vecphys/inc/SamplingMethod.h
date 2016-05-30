f1e148f7b2a27d88b4d2d1abf6bb144154537466
mb <marilena.bandieramonte@cern.ch>
2016-05-30 21:15:51 +0200

    [GEANT-202] Correction in the (px,py,pz) momentum unit conversion from GeantV to VecPhys and few new methods added to the VecPhysOrchestrator

--- vecphys/inc/SamplingMethod.h
+++ vecphys/inc/SamplingMethod.h
@@ -5,10 +5,10 @@ namespace vecphys {
 
 enum SamplingMethod {
   kNullMethod = -1,
-  kAlias,                //Alias method
-  kRejection,            //Geant4 CompositionAndRejection
-  kUnpack,               //Shuffling
-  kNumberSamplingMethod
+  kAlias,                //Alias method [0]
+  kRejection,            //Geant4 CompositionAndRejection [1]
+  kUnpack,               //Shuffling [2]
+  kNumberSamplingMethod  //[3]
 };
 
 } // end namespace vecphys
