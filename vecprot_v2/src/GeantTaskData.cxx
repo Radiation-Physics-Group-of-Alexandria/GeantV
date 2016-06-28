8f211f2cc07feaa6dbf7e8848a7babd5b3cf5dbf
John Apostolakis <john.apostolakis@cern.ch>
2016-06-28 20:18:55 +0200

    Fixes for problems from my trial rebase.

--- vecprot_v2/src/GeantTaskData.cxx
+++ vecprot_v2/src/GeantTaskData.cxx
@@ -27,7 +27,7 @@ GeantTaskData::GeantTaskData(size_t nthreads, int maxDepth, int maxPerBasket)
       fSOA3Dworkspace2(new vecgeom::SOA3D<vecgeom::Precision>(5 * maxPerBasket)),
       fFieldPropagator(nullptr), fFieldObj(nullptr),
       fSizeInt(5 * maxPerBasket), fIntArray(new int[fSizeInt]), fTransported(nullptr), fNkeepvol(0),
-      fNsteps(0), fNsnext(0), fNphys(0), fNmag(0), fNpart(0), fNsmall(0), fNcross(0)
+      fNsteps(0), fNsnext(0), fNphys(0), fNmag(0), fNpart(0), fNsmall(0) // , fNcross(0)
 {
   // Constructor
   fNthreads = nthreads;
@@ -62,7 +62,7 @@ GeantTaskData::GeantTaskData(void *addr, size_t nthreads, int maxDepth, int maxP
       fSOA3Dworkspace1(nullptr),
       fSOA3Dworkspace2(nullptr),
       fSizeInt( 5*maxPerBasket ), fIntArray( nullptr ), fTransported(nullptr), fNkeepvol(0),
-      fNsteps(0), fNsnext(0), fNphys(0), fNmag(0), fNpart(0), fNsmall(0), fNcross(0)
+      fNsteps(0), fNsnext(0), fNphys(0), fNmag(0), fNpart(0), fNsmall(0) // , fNcross(0)
 {
   // Constructor
   char *buffer = (char*)addr;
